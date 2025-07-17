a5877e216c076413750eb5a24d91c735fc934862
Swarit <swaritks@student.ubc.ca>
2025-07-17 16:57:59 -0700

    Loaded Images and Transformed into Tensors

--- src/load_data.py
+++ src/load_data.py
@@ -1,4 +1,15 @@
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
-transform = transforms.ToTensor()
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),   # Make all images 224x224
+    transforms.ToTensor()
+])
+
+dataset = ImageFolder(root="data", transform=transform)                 # Data is found within "data" folder, transform converts each image
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)           # Load images in groups of 32 to increase GPU efficiency
+
+images, labels = next(iter(dataloader))                                 # next(iter(dataloader)) grabs the first batch of images
+print(images.shape, labels.shape)                                       # ensuring size and shape are consistent
