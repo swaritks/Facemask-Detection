6e03ec1012be271422c8771c914bbf98bc7b7e90
Swarit <swaritks@student.ubc.ca>
2025-07-17 22:34:13 -0700

    Sucessfully Ran the Model Over 5 Epochs

--- src/train.py
+++ src/train.py
@@ -1,5 +1,34 @@
 import torch
 from models.cnn import CNN
+from src.load_data import train_loader, val_loader
+
+import torch.nn as nn
+import torch.optim as optim
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = CNN().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 5
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+
+    for images, labels in train_loader: 
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() 
+
+    print(f"Epoch {epoch+1}, Loss: {running_loss:.4f}")
+    
+torch.save(model.state_dict(), "models/facemask_cnn.pth")
